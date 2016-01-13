ade01c84d532b47ab5ac9a50375cf8051cb7a24e
Tony <ntutonycao@gmail.com>
2016-01-13 16:33:37 +0800

    Added content.

--- MemoryUseageTest/src/TestMain.java
+++ MemoryUseageTest/src/TestMain.java
@@ -1,3 +1,4 @@
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Date;
@@ -5,54 +6,82 @@ import java.util.Enumeration;
 import java.util.Hashtable;
 import java.util.LinkedList;
 import java.util.TreeMap;
- 
 
 public class TestMain {
 	
-	public static void main(String args[]){
+	private static byte nonLRC = 0x0D;
+	private static byte prefix = 0x02;
+	private static byte suffix = 0x03;
+	
+	public static void main(String args[]) {
 		
 		
-		LinkedList list=new LinkedList();
-		ArrayList arr=new ArrayList();
- 
- 
-		  System.out.println("MB: " + (double) (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / 1024/1024);
-		  
-
-		  TreeMap table=new TreeMap();
-		//4000000
-		for(int i=0;i<4000;i++){
-			TreeMap subTable=new TreeMap();
-		for(int j=0;j<100;j++){
-		TestObj testObj=new TestObj();
-		testObj.id=j;
-		testObj.type="fdsfaf";
-		testObj.date=new Date();
-		subTable.put(j, testObj);
-			}
-		table.put(i,subTable);
+		
+		
+		String middleMessage="fds";
+		
+		byte[] sending=new byte[middleMessage.length()+3];
+		sending[0]=prefix;
+		byte[] middleByte=middleMessage.getBytes();
+		for(int i=0;i<middleByte.length;i++){
+			sending[i+1]=middleByte[i];
 		}
- 
 		
-//		System.out.println("loop the table:");
-//		while(e.hasMoreElements()){
-//			int key=(int) e.nextElement();
-//			System.out.println(key+" "+((TestObj)table.get(key)).id);
-//		}
+		sending[sending.length-2]=suffix;
+		sending[sending.length-1]=nonLRC;
 
-	        System.out.println("MB: " + (double) (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / 1024/1024);
-	  
+		
+		for(byte tmp:sending){
+			System.out.print(tmp+" ");
+		}
 		
 		
+		
+		
+
+		byte[] message = new byte[] { 0x02, (byte) 0xB3, 0x43, 0x4E, (byte) 0xB3, 0x31, (byte) 0xB3, 0x45, (byte) 0xB3,
+				0x31, 0x30, 0x31, (byte) 0xB3, 0x10,  };
+		String str2 = new String(message, StandardCharsets.UTF_8);
+		System.out.println("send:" + str2);
+		byte[] loop="\r".getBytes();
+		for(byte tmp:loop){
+			System.out.println(tmp);
+		}
+		System.out.println("the huan hang:"+ 		"0DH".getBytes()[2]);
+
+		LinkedList list = new LinkedList();
+		ArrayList arr = new ArrayList();
+
+		System.out.println("MB: "
+				+ (double) (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / 1024 / 1024);
+
+		TreeMap table = new TreeMap();
+		// 4000000
+		for (int i = 0; i < 4000; i++) {
+			TreeMap subTable = new TreeMap();
+			for (int j = 0; j < 100; j++) {
+				TestObj testObj = new TestObj();
+				testObj.id = j;
+				testObj.type = "fdsfaf";
+				testObj.date = new Date();
+				subTable.put(j, testObj);
+			}
+			table.put(i, subTable);
+		}
+
+		// System.out.println("loop the table:");
+		// while(e.hasMoreElements()){
+		// int key=(int) e.nextElement();
+		// System.out.println(key+" "+((TestObj)table.get(key)).id);
+		// }
+
+		System.out.println("MB: "
+				+ (double) (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / 1024 / 1024);
+
 	}
-	
-	public void test(){
- 
-		
+
+	public void test() {
+
 	}
-	
-	
-	
-	
-	
+
 }
